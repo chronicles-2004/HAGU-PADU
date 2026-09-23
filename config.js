395fa2f51f3a2a5cbd19bb64772f5915ff3ce6e4
chronicles-2004 <adhikarysagar2004@gmail.com>
2026-09-23 12:40:49 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,79 @@
+
+
+const SITE_CONFIG = {
+
+  /* -----------------------------------------
+     AUTHENTICATION DATES
+  ----------------------------------------- */
+
+  birthday: "25/09/2005",
+
+  firstMeeting: "03/05/2026",
+
+
+  /* -----------------------------------------
+     PHOTOS
+
+     assets folder-এর ভিতরের ছবি এখানে add করবি.
+  ----------------------------------------- */
+
+  photos: [
+
+    "assets/photo1.jpg",
+
+    "assets/photo2.jpg",
+
+    "assets/photo3.jpg",
+
+    "assets/photo4.jpg",
+
+    "assets/photo5.jpg",
+
+    "assets/photo6.jpg",
+
+    "assets/photo7.jpg",
+
+    "assets/photo8.jpg",
+
+    "assets/photo9.jpg"
+
+  ],
+
+
+  /* -----------------------------------------
+     PHOTO CAPTIONS
+  ----------------------------------------- */
+
+  captions: [
+
+    "আমার প্রথম favourite smile",
+
+    "এই হাসিটা রেখে দিস",
+
+    "আমার brave Kuchupuchu 🥹",
+
+    "আমার চোখে তুই",
+
+    "আমার Pagli ❤️",
+
+    "একটা সুন্দর মুহূর্ত",
+
+    "তুই, তোর মতোই",
+
+    "Just you.",
+
+    "আর একটা reason to smile"
+
+  ]
+
+};
+
+
+/*
+   Compatibility variable.
+
+   app.js এই variable ব্যবহার করবে.
+*/
+
+const photos = SITE_CONFIG.photos;
+
